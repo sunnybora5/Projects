1574cbaccda97d822ba6f6dc59cc4873740c6d5c
Iamrhema <48246378+Iamrhema@users.noreply.github.com>
2019-09-02 11:25:38 +0200

    binary
    this converts binary code to numbers

---
+++ Python/Binary.py
@@ -0,0 +1,6 @@
+#this is a program to convert binary numbers to decimal
+#input for binary number
+binary = input("Enter number in binary format: ")
+decimal = int(binary, 2)
+#connverting from binary to decimal
+print(binary, "in decimal =" ,decimal)
